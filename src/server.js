4a49d3be9fb599f73fdd970a48cf53733b6710f6
João Guilherme Pandolfo <jguilherme1999@gmail.com>
2021-06-23 14:41:38 -0300

    config routes

--- src/server.js
+++ src/server.js
@@ -1,4 +1,8 @@
 const express = require("express");
+const routes = require("./route")
 const app = express();
+
+app.use(express.json())
+app.use(routes)
 
 app.listen(3333, () => console.log("Servidor rodando!"))
